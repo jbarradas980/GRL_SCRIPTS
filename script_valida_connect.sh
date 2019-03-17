b2bd878f33b32de169fe849f41472252de1d00c1
jbarradas980 <47677310+jbarradas980@users.noreply.github.com>
2019-03-17 03:08:34 -0600

    Update script_valida_connect.sh

--- script_valida_connect.sh
+++ script_valida_connect.sh
@@ -2,7 +2,7 @@
 #Este script permite validar la cantidad de hosts en una red a la que se le puede hacer ping desde .1 a .254.
 #Se pueden cambiar los siguientes valores para definir donde inicia la detección de hosts y donde termina.
 INICIA_PING=1
-FIN_PING=254
+FIN_PING=100
 #Se define el tiempo maximo del ping
 MAX_TIME=2
 
@@ -11,7 +11,7 @@ MAX_TIME=2
 IP_ADDR=$1
 DIR_RED=$(echo $IP_ADDR | egrep -o ^[0-9]{1\,3}\.[0-9]{1\,3}\.[0-9]{1\,3}\.)
 
-[[ $DIR_RED ]] || { echo "Ocurrió un error con la dirección de red $IP_ADDR"; }
+[[ $DIR_RED ]] || { echo "Ocurrió un error con la dirección de red $IP_ADDR"; exit 1; }
 
 echo "Iniciando detección de dispositivos que se puede hacer ping..."
 echo "Este proceso podría tardar unos minutos"
@@ -23,4 +23,3 @@ for host in $(seq $INICIA_PING $FIN_PING); do
 	#Se imprime en pantalla el dispositivo al cual se establece el ping
 	[[ $RESULTADO_ENT -eq 1 ]] && { echo "$DIR_RED$host"; } 
 done
-
